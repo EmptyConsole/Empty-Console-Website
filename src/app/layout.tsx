9a164d60afd5ec2dd7c4619660b00f7de057fb58
ConsoleEmpty <shyguygamedev@gmail.com>
2025-12-19 23:09:18 -0800

    link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
     title: "Empty Console | Top Grade Software Development Team",
     description: "Empty Console is a team of students who came together due to their love of programming.",
     type: "website",
+    images: [
+      {
+        url: "/EmptyConsoleLogo.png",
+        width: 1200,
+        height: 630,
+        alt: "Empty Console Logo",
+      },
+    ],
   },
   icons: {
     icon: "/EmptyConsoleLogo.png",
